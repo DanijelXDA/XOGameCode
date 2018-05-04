e532e20e4cefd7c741b2bc4a53bf8f57e9a3b318
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-05-04 16:04:48 +0200

    Update source code to v1
    What's new: 
        -> Finalize code
        -> Bug fixes 
        -> Remove useless stuff

--- XO.hpp
+++ XO.hpp
@@ -1,20 +1,24 @@
-#ifndef _XO_INCLUDED_HPP
-#define _XO_INCLUDED_HPP
+#ifndef XO_HPP_INCLUDED
+#define XO_HPP_INCLUDED
+
+#include <iostream>
+using namespace std;
 
 #define PRAZNO 0
 #define IGRAC_X 1
 #define IGRAC_Y 2
 #define KRAJ_IGRE 3
 
-class XO {
+class XO{
     private:
-       int tabla[3][3];
-       int igrac;
+        int tabla[3][3];
+        int igrac;
     public:
-       XO();
-       void odigrajPotez() const;
-       void prikaziTablu() const;
-       bool kraj()const;
+        XO();
+        int getIgrac() const;
+        void odigrajPotez(int);
+        void prikaziTablu() const;
+        bool kraj() const;
 };
 
-#endif
+#endif // XO_HPP_INCLUDED
