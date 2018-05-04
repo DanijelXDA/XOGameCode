a99b2f451f6d64724d413368ddde9ad02fac36bb
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-05-04 16:00:47 +0200

    Update source code to v1
    What's new:
        -> Finalize code
        -> Bug fixes
        -> Remove useless stuff

--- main.cpp
+++ main.cpp
@@ -1,43 +1,63 @@
-#include <iostream>
 #include "XO.hpp"
 
-using namespace std;
+int potezi(int igrac){
+    if(igrac == IGRAC_X){
+        cout << "Na potezu je igrac X (iks)" << endl;
+    }else{          // O
+        cout << "Na potezu je igrac O (oks)" << endl;
+    }
+    cout << "Izaberite poziciju (dozvoljene vrednosti 1-9): " << endl;
+    cout << "\t-------------" << endl;
+    cout << "\t| 1 | 2 | 3 |" << endl;
+    cout << "\t-------------" << endl;
+    cout << "\t| 4 | 5 | 6 |" << endl;
+    cout << "\t-------------" << endl;
+    cout << "\t| 7 | 8 | 9 |" << endl;
+    cout << "\t-------------" << endl << endl;
+    cout << "> ";
 
-void meni() {
-   int izbor;
-    
-    XO xo;
-    
-    do {
-        cout << endl;
-        cout << "1. prikazi tablu" << endl;
-        cout << "2. odigraj potez" << endl;
-        cout << "3. kraj rada programa" << endl;
-        
-        cout << "Izaberite operaciju: ";
-        cin >> izbor;
-        
-        XO();
-        
-        switch(izbor){
-            case 1: xo.prikaziTablu();
-            break;
-            
-            case 2: xo.odigrajPotez();
-            break;
-            
-            case 3: xo.kraj();
-        }
-        
-    }while(izbor != 3);
-    
-    return;
+    int pozicija;
+    cin >> pozicija;
+
+    if(pozicija < 1 || pozicija > 9){
+        cout << "Izabrana pozicija ne postoji" << endl;
+    }
+
+    return pozicija;
 }
 
-int main(void)
+int meni(){
+    cout << "1. prikazi tablu" << endl;
+    cout << "2. odigraj potez" << endl;
+    cout << "3. kraj rada programa" << endl;
+    cout << "Izaberite operaciju: ";
+    int i;
+    cin >> i;
+    if(i < 1 || i > 3){
+        cout << "Izabrana operacija ne postoji" << endl << endl;
+    }
+    return i;
+}
 
-{
-    meni();
-    
+int main(){
+
+    XO partija;
+    int operacija = 0;
+
+    while(operacija != 3 && !partija.kraj()){
+        operacija = meni();
+        switch(operacija){
+            case 1: // prikazivanje tabele
+                partija.prikaziTablu();
+                break;
+            case 2: // povlacenje poteza
+                partija.odigrajPotez( potezi(partija.getIgrac()) );
+                break;
+            case 3: // kraj rada programa
+                cout << "Kraj programa..." << endl;
+                break;
+        }
+    }
+
     return 0;
 }
